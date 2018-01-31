792f66180b3a22f97f98614012a11b3996f92eda
hpsgts@live.com <Sa123456>
2018-01-31 21:17:12 +0800

    初始化项目基础架构

---
+++ hpcloud-common/src/main/java/com/hpsgts/hpcloud/common/vo/Response.java
@@ -0,0 +1,72 @@
+package com.hpsgts.hpcloud.common.vo;
+
+import com.hpsgts.hpcloud.common.enums.ResponseEnum;
+
+/**
+ * @author 黄朴（Herper.Plain）
+ * @Date 2018/1/30 下午2:27
+ * @Company 上海宏鹿信息技术服务有限公司
+ */
+public class Response {
+    private int code;
+    private String message;
+    private Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Response code(int code){
+        this.code = code;
+        return this;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Response message(String message){
+        this.message = message;
+        return this;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    public Response date(Object data){
+        this.data = data;
+        return this;
+    }
+
+    public Response(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+    public Response(ResponseEnum responseEnum) {
+        this.code = responseEnum.getCode();
+        this.message = responseEnum.getMessage();
+    }
+    public Response() {
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
