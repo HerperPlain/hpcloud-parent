2832d67ec1b1936d208729ee19bd242e9fbcc011
hpsgts@live.com <Sa123456>
2018-01-31 21:28:42 +0800

    初始化项目基础架构:添加服务中心

---
+++ hpcloud-services/src/main/java/com/hpsgts/hpcloud/services/HpcloudServicesApplication.java
@@ -0,0 +1,12 @@
+package com.hpsgts.hpcloud.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcloudServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpcloudServicesApplication.class, args);
+	}
+}
